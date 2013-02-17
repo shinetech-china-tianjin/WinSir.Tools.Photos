2a4827342e5124f845534950e801a8876aa0076c
Lu Rongkai <lurongkai@gmail.com>
2013-02-17 23:18:30 +0800

    add io interface.

---
+++ WinSir.Tools.Photos/IBasicIO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinSir.Tools.Photos
+{
+    internal interface IBasicIO
+    {
+        IBasicIO Rename(string newName);
+    }
+}
