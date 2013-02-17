d6e7edc43deff0f172c6bf9665acedb5fd3a7746
Lu Rongkai <lurongkai@gmail.com>
2013-02-17 22:59:06 +0800

    ready to design new fluent api for io composition.

---
+++ WinSir.Tools.Photos/IOHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinSir.Tools.Photos
+{
+    internal class IOHelper
+    {
+    }
+}
---
+++ WinSir.Tools.Photos/Migration/RenamedByExif.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace WinSir.Tools.Photos
+{
+    public class RenamedByExif
+    {
+        public string SourceFolder { get; set; }
+        public string DestinantionFolder { get; set; }
+
+        public void Execute()
+        {
+            var files = Directory.GetFiles(SourceFolder, "*.jpg");
+            foreach (var file in files)
+            {
+                try
+                {
+                    DateTime dt;
+                    using (var em = new ExifManager(file))
+                    {
+                        dt = em.DateTimeOriginal;
+                    }
+
+                    if (dt == DateTime.MinValue) continue;
+
+                    var fi = new FileInfo(file);
+                    var newName = Path.Combine(DestinantionFolder,
+                                               string.Format("{0}.jpg", dt.ToString("yyyy-MM-dd_HH.mm.ss")));
+                    fi.MoveTo(newName);
+                }
+                catch
+                {
+                }
+            }
+
+        }
+    }
+}
